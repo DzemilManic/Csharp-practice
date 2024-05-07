7f9a57c866ba8a15ae0521660411041f687ad247
Džemil Manić <dzemilmanic@hotmail.com>
2024-05-07 11:49:13 +0200

    Create Developer.cs

---
+++ Domaci 1/Developer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CHAT.EmployeeManager;
+
+namespace CHAT
+{
+    public class Developer : Employee
+    {
+        public Developer(int iD, string firstName, string lastName, double salary)
+        {
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+        }
+
+        public override void SendMessage(string message)
+        {
+            Console.WriteLine("Poruka poslata Developeru:");
+            Console.WriteLine($"{FirstName} {LastName} (DEV) : {message}");
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Developer developer &&
+                   ID == developer.ID &&
+                   FirstName == developer.FirstName &&
+                   LastName == developer.LastName &&
+                   Salary == developer.Salary &&
+                   ID == developer.ID &&
+                   FirstName == developer.FirstName &&
+                   LastName == developer.LastName &&
+                   Salary == developer.Salary;
+        }
+    }
+}
