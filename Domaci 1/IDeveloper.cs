a18c8f04d87068edcffa21342eccea6615763245
Džemil Manić <dzemilmanic@hotmail.com>
2024-05-07 11:49:18 +0200

    Create IDeveloper.cs

---
+++ Domaci 1/IDeveloper.cs
@@ -0,0 +1,10 @@
+﻿namespace CHAT
+{
+    public interface IDeveloper
+    {
+        event EmployeeManager.MessageEventHandler MessageReceived;
+
+        bool Equals(object obj);
+        void SendMessage(int recipientId, string message);
+    }
+}
