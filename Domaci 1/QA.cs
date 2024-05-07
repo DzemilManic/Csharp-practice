ca1d49739073808dca900ba494c8cefe8d06351a
Džemil Manić <dzemilmanic@hotmail.com>
2024-05-07 11:49:40 +0200

    Create QA.cs

---
+++ Domaci 1/QA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CHAT.EmployeeManager;
+
+namespace CHAT
+{
+    public class QA : Employee
+    { 
+        public QA(int iD, string firstName, string lastName, double salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+            ID = iD;
+        }
+        public override void SendMessage(string message)
+        {
+            Console.WriteLine("Poruka poslata QA:");
+            Console.WriteLine($"{FirstName} {LastName} (QA) : {message}");
+        }
+    }
+}
