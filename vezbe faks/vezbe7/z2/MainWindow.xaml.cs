af68c540cf97f5601a52f895e5a5a733cd128c7b
Džemil Manić <dzemilmanic@hotmail.com>
2024-07-05 17:02:11 +0200

    z2

---
+++ vezbe faks/vezbe7/z2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace z2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var root = new stavka();
+            var d1 = new stavka() { Naziv = "Dedo" };
+            var b1 = new stavka() { Naziv = "babo" };
+            var j1 = new stavka() { Naziv = "ja" };
+            var j2 = new stavka(){ Naziv = "brat"};
+            var j3 = new stavka() { Naziv = "sestra" };
+            b1.Podstavke.Add(j1);
+            b1.Podstavke.Add(j2);
+            b1.Podstavke.Add(j3);
+            var b2 = new stavka() { Naziv = "babov brat" };
+            var b3 = new stavka() { Naziv = "babov drugi brat" };
+            d1.Podstavke.Add(b1);
+            d1.Podstavke.Add(b2);
+            d1.Podstavke.Add(b3);
+            var d2 = new stavka() { Naziv = "Dedov brat" };
+            root.Podstavke.Add(d1);
+            root.Podstavke.Add(d2);
+            trvStavke.Items.Add(root);
+        }
+    }
+}
---
+++ vezbe faks/vezbe7/z2/stavka.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z2
+{
+    internal class stavka
+    {
+        public stavka() {
+            Podstavke = new List<stavka>();
+        }
+        public string Naziv  { get; set; }
+        public List<stavka> Podstavke { get; set; }
+    }
+}
