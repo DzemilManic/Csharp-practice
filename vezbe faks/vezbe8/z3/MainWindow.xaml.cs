a99b1ca2b0ec0dd470f889251bb82012f952a571
Džemil Manić <dzemilmanic@hotmail.com>
2024-07-08 11:17:52 +0200

    z3

---
+++ vezbe faks/vezbe8/z3/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace z3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Student> Studenti { get; set; }
+        public Student StudentiZaDodavanje { get; set; }
+        public MainWindow()
+        {
+            Studenti = new ObservableCollection<Student>
+            {
+                new Student  { Ime = "Merisa", Prezime = "Besirovic", Telefon = "0652312456", GodinaStudija = 2 },
+                new Student  { Ime = "Merisa", Prezime = "Besirovic", Telefon = "0652312456", GodinaStudija = 2 },
+                new Student  { Ime = "Merisa", Prezime = "Besirovic", Telefon = "0652312456", GodinaStudija = 2 }
+            };
+            StudentiZaDodavanje = new Student();
+            InitializeComponent();
+        }
+    }
+}
---
+++ vezbe faks/vezbe8/z3/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z3
+{
+    public class Student : INotifyPropertyChanged
+    {
+        private string ime;
+        private string prezime;
+        private string telefon;
+        private int godinaStudija;
+
+        public int GodinaStudija
+        {
+            get { return godinaStudija; }
+            set
+            {
+                godinaStudija = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Telefon
+        {
+            get { return telefon; }
+            set { telefon = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Prezime
+        {
+            get { return prezime; }
+            set { prezime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
