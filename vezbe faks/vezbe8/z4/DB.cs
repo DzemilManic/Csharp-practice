9f77d809cdb88ebc7b3c8a83ed597b8d50b4c4c0
Džemil Manić <dzemilmanic@hotmail.com>
2024-07-08 11:18:08 +0200

    z4

---
+++ vezbe faks/vezbe8/z4/DB.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace z4
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+    public class DB
+    {
+        private OleDbConnection conn = new OleDbConnection();
+        private OleDbCommand cmd;
+        public DB() {
+            conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:data;Persist Security Info=False;";
+            cmd = conn.CreateCommand();
+        }
+        public void Create(Person p)
+        {
+            try
+            {
+                conn.Open();
+                string query = $"INSERT INTO Table1 VALUE ({p.FirstName}, {p.LastName}, {p.PhoneNumber}, {p.Email})";
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally 
+            { 
+                if(conn!= null)
+                   conn.Close(); 
+            }
+        }
+        public DataTable GetAll()
+        {
+            
+            DataTable dt = new DataTable();
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Table1";
+                cmd.CommandText = query;
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                da.Fill(dt);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            { 
+                if(conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return dt;
+
+        }
+        public void Update(Person p) { }
+        public void Delete(Person p) { }
+
+    }
+}
---
+++ vezbe faks/vezbe8/z4/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace z4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DB db;
+        public ObservableCollection<Person> Persons { get; set; }
+        public MainWindow()
+        {
+            db = new DB();
+            pokupiPodatke();
+            InitializeComponent();
+        }
+        private void pokupiPodatke()
+        {
+            var dt = db.GetAll();
+            foreach (var item in dt.Rows) {
+                Persons.Add(
+                    new Person
+                    {
+                        FirstName = item[0].ToString(),
+                    });
+            }
+        }
+    }
+}
